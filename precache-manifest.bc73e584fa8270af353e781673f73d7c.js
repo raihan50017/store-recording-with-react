1dabe51b95619b2779410526ad087730256ebb3d
RAIHAN <aburaihan50017@gmail.com>
2020-07-09 11:37:24 +0600

    store recording finished

---
+++ precache-manifest.bc73e584fa8270af353e781673f73d7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3dcafa12c7cce194ade8d8870cdf6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dcea5550f75be01f072",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8946415129a681f840ad",
+    "url": "/static/css/main.71035ef6.chunk.css"
+  },
+  {
+    "revision": "4dcea5550f75be01f072",
+    "url": "/static/js/2.204eddd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.204eddd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8946415129a681f840ad",
+    "url": "/static/js/main.fc0e5187.chunk.js"
+  },
+  {
+    "revision": "22f92c13c2c64a65e9d5",
+    "url": "/static/js/runtime-main.4db7ebc6.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
